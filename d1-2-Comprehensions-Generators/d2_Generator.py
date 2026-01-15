04f8bb8c66f56b72e924f3284c5deb677410f9c7
LODNight <khuunhattin123@gmail.com>
2026-01-15 23:07:35 +0700

    Day 2: Generatos

--- d1-2-Comprehensions-Generators/d2_Generator.py
+++ d1-2-Comprehensions-Generators/d2_Generator.py
@@ -7,3 +7,15 @@
 my_gen = (i for i in range(10000000))
 
 print(my_gen)
+
+# Bài tập maaxu: Chuyển đổi cú pháp (Dễ)
+# Đề bài: Bạn có đoạn code List Comprehension dưới đây để tính bình phương. Hãy viết lại nó dưới dạng Generator Expression và dùng vòng lặp for để in ra từng số.
+
+# Input: numbers = [1, 2, 3, 4, 5]
+
+numbers = [1, 2, 3, 4, 5]
+
+num = (i**3 for i in numbers)
+
+for i in num:
+    print(i)
--- d2_ex_1.py
+++
@@ -1,6 +0,0 @@
-# Bài tập 1: Chuyển đổi cú pháp (Dễ)
-# Đề bài: Bạn có đoạn code List Comprehension dưới đây để tính bình phương. Hãy viết lại nó dưới dạng Generator Expression và dùng vòng lặp for để in ra từng số.
-
-# Input: numbers = [1, 2, 3, 4, 5]
-
-numbers = [1, 2, 3, 4, 5]
